558eff9950c3bab008bd470d20017100126174d1
liwei <liwei@bbbao.com>
2014-09-24 13:04:33 +0800

    add browse

--- sql/insert.sql
+++ sql/insert.sql
@@ -6,4 +6,19 @@ delete from browse;
 insert into browse 
 (browse_id, browse_value, description,dt_created, dt_updated)
 values
-(1,'阅读','精品阅读',now(), now());
+(1,'情感治愈','关于情感类的阅读',now(), now());
+
+insert into browse
+(browse_id, browse_value, description, dt_created, dt_updated)
+values
+(2,'经典散文','优美经典的优秀散文', now(), now());
+
+insert into browse
+(browse_id, browse_value, description, dt_created, dt_updated)
+values
+(3,'心灵鸡汤','激发正能量的文章', now(), now());
+
+insert into browse
+(browse_id, browse_value, description, dt_created, dt_updated)
+values
+(4,'幽默笑话','经典的笑话收录', now(), now());
