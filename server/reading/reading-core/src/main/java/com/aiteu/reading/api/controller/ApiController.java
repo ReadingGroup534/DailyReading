8fd6cc3954b8647743fdcb15cdf4ba1864535f8f
liwei <tianya1437@gmail.com>
2014-10-19 11:46:55 +0800

    add md5 tools

--- server/reading/reading-core/src/main/java/com/aiteu/reading/api/controller/ApiController.java
+++ server/reading/reading-core/src/main/java/com/aiteu/reading/api/controller/ApiController.java
@@ -2,17 +2,13 @@ package com.aiteu.reading.api.controller;
 
 import java.util.Map;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.aiteu.reading.api.service.ApiService;
-import com.aiteu.reading.core.JsonConvertUtils;
 /**
  * 提供所有的api接口
  * 1.api/browse 文章分类
@@ -40,11 +36,5 @@ public class ApiController {
 		modelMap.put("words", "Welcome visit!");
 		
 		return "browse.htm";
-	}
-	
-	@RequestMapping("/api/daily")
-	public void apiDailyList(HttpServletRequest req, HttpServletResponse res){
-		Map<String, Object> data = apiService.getDailyAiticle(req);
-		JsonConvertUtils.outJson(res, data);
 	}
 }
