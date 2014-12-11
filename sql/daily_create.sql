2a0273d6b50891e13aa0595543680097748ff911
liwei <liwei@bbbao.com>
2014-12-11 16:41:38 +0800

    please read README.md

--- sql/daily_create.sql
+++ sql/daily_create.sql
@@ -1,1 +1,10 @@
+/*************************
+ * create database for the reading
+ * and add app user
+**************************/
 create database if not exists reading default charset = utf8 default collate = utf8_general_ci;
+/******************************
+* grant
+*******************************/
+grant all privileges on reading.* to app@localhost identified by 'app_pass';
+grant all privileges on reading.* to app@'%' identified by 'app_pass';
